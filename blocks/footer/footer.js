588ca4e2465ba205aded82c581dc2c997a7576b5
Sweta Barman <swbarman@adobe.com>
2025-02-12 08:26:11 -0500

    feat: initial footer changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -15,6 +15,15 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
   block.append(footer);
+
+  document.querySelectorAll('footer .footer .default-content-wrapper ul:first-child li').forEach((li) => {
+    const picture = li.querySelector('picture');
+    const link = li.querySelector('a');
+
+    if (picture && link) {
+      link.textContent = '';
+      link.insertBefore(picture, link.firstChild);
+    }
+  });
 }
