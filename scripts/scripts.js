713fe5ecd9281f785d7bd5fa6325da5e20f840e2
Charity Helms <3883795+helms-charity@users.noreply.github.com>
2025-02-13 08:27:40 -0500

    WIP add templating, parallax section styles in prog (#58)
    * add tmplating, parallax section styles in prog
    
    * CSS updates for parallax sections
    
    * add intersection observer for animations
    
    * linting
    
    * revert inventory.json
    
    ---------
    
    Co-authored-by: Charity Helms <chelms@adobe.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,8 +10,11 @@ import {
   waitForFirstImage,
   loadSection,
   loadSections,
-  loadCSS,
+  loadCSS, getMetadata, toClassName,
 } from './aem.js';
+
+const TEMPLATES = ['home']; // add your templates here
+const TEMPLATE_META = 'template';
 
 /**
  * Builds hero block and prepends to main in a new section.
@@ -41,6 +44,97 @@ async function loadFonts() {
 }
 
 /**
+ * Sets an optimized background image for a given section element.
+ * This function takes into account the device's viewport width and device pixel ratio
+ * to choose the most appropriate image from the provided breakpoints.
+ *
+ * @param {HTMLElement} section - The section element to which the background image will be applied.
+ * @param {string} bgImage - The base URL of the background image.
+ * @param {Array<{width: string, media?: string}>} [breakpoints=[
+ *  { width: '450' },
+ *   { media: '(min-width: 450px)', width: '750' },
+ *   { media: '(min-width: 768px)', width: '1024' },
+ *   { media: '(min-width: 1024px)', width: '1600' },
+ *   { media: '(min-width: 1600px)', width: '2200' },
+ * ]] - An array of breakpoint objects. Each object contains a `width` which is the width of the
+ * image to request, and an optional `media` which is a media query string indicating when this
+ * breakpoint should be used.
+ */
+
+export const resizeListeners = new WeakMap();
+export function getBackgroundImage(element) {
+  const sectionData = element.dataset.background;
+  const bgImages = sectionData.split(',').map((img) => img.trim());
+  return (bgImages.length === 1
+    || (window.innerWidth > 1024 && bgImages.length === 2)) ? bgImages[0] : bgImages[1];
+}
+
+export function createOptimizedBackgroundImage(element, breakpoints = [
+  { width: '450' },
+  { media: '(min-width: 450px)', width: '768' },
+  { media: '(min-width: 768px)', width: '1024' },
+  { media: '(min-width: 1024px)', width: '1600' },
+]) {
+  const updateBackground = () => {
+    const bgImage = getBackgroundImage(element);
+    const { pathname } = new URL(bgImage, window.location.href);
+
+    const matchedBreakpoint = breakpoints
+      .filter((br) => !br.media || window.matchMedia(br.media).matches)
+      .reduce((acc, curr) => (parseInt(curr.width, 10)
+      > parseInt(acc.width, 10) ? curr : acc), breakpoints[0]);
+
+    const adjustedWidth = matchedBreakpoint.width * window.devicePixelRatio;
+    element.style.backgroundImage = `url(${pathname}?width=${adjustedWidth}&format=webply&optimize=highest)`;
+  };
+
+  if (resizeListeners.has(element)) {
+    window.removeEventListener('resize', resizeListeners.get(element));
+  }
+  resizeListeners.set(element, updateBackground);
+  window.addEventListener('resize', updateBackground);
+  updateBackground();
+}
+
+function decorateStyledSections(main) {
+  Array.from(main.querySelectorAll('.section[data-background]')).forEach((section) => {
+    createOptimizedBackgroundImage(section);
+  });
+}
+
+// Initiate the IntersectionObserver to animate the parallax sections
+function createObserver() {
+  const parallaxRight = document.querySelector('.parallax.right');
+  const parallaxLeft = document.querySelector('.parallax.left');
+  document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (entry.target.classList.contains('right')) {
+            entry.target.querySelector('div').classList.add('slideLeft');
+          } else if (entry.target.classList.contains('left')) {
+            entry.target.querySelector('div').classList.add('slideRight');
+          }
+        } else if (entry.target.classList.contains('right')) {
+          entry.target.querySelector('div').classList.remove('slideLeft');
+        } else if (entry.target.classList.contains('left')) {
+          entry.target.querySelector('div').classList.remove('slideRight');
+        }
+      });
+    }, {
+      threshold: 0.3,
+    });
+
+    [parallaxRight, parallaxLeft].forEach((section) => {
+      if (section) {
+        observer.observe(section);
+      }
+    });
+  });
+}
+createObserver();
+
+/**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
@@ -65,6 +159,30 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateStyledSections(main);
+  createObserver();
+}
+
+/**
+ * load the template specific js and css
+ */
+async function loadTemplate(main) {
+  try {
+    const template = getMetadata(TEMPLATE_META) ? toClassName(getMetadata(TEMPLATE_META)) : null;
+    if ((template && TEMPLATES.includes(template))) {
+      const templateJS = await import(`../templates/${template}/${template}.js`);
+      // invoke the default export from template js
+      if (templateJS.default) {
+        await templateJS.default(main);
+      }
+      loadCSS(
+        `${window.hlx.codeBasePath}/templates/${template}/${template}.css`,
+      );
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(`Failed to load template with error : ${err}`);
+  }
 }
 
 /**
@@ -77,6 +195,7 @@ async function loadEager(doc) {
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
+    await loadTemplate(main);
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
