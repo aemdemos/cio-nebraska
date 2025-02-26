5d8ac265a343e53d4de7cde65359ad697322f79d
Charity Helms <chelms@adobe.com>
2025-02-26 12:48:42 -0500

    add siteImprov analytics

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -1,1 +1,17 @@
 // add delayed functionality here
+
+async function enableSiteImprove() {
+  const siteImproveScript = document.createElement('script');
+  siteImproveScript.type = 'text/javascript';
+  siteImproveScript.innerHTML = `
+/*<![CDATA[*/
+(function() {
+var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+sz.src = '//siteimproveanalytics.com/js/siteanalyze_6044960.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/`;
+  siteImproveScript.async = true;
+  document.body.append(siteImproveScript);
+}
+enableSiteImprove();
