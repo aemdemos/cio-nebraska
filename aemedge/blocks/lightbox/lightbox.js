e60be27198d89ebfe2069259ad80c2b2b69bd3a1
Zouhir Dahbi <zdahbi@adobe.com>
2025-02-26 13:59:15 -0500

    trying a different logic for every img link

--- aemedge/blocks/lightbox/lightbox.js
+++ aemedge/blocks/lightbox/lightbox.js
@@ -2,32 +2,29 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { Modal } from '../form/components/modal/modal.js';
 
 export default function decorate(block) {
-  // Find all pictures followed by links to #
-  block.querySelectorAll('picture').forEach((picture) => {
-    const nextElement = picture.nextElementSibling;
-    if (nextElement && nextElement.tagName === 'A' && nextElement.getAttribute('href') === '#') {
+  // Find all links that contain pictures
+  document.querySelectorAll('a:has(picture)').forEach((link) => {
+    const picture = link.querySelector('picture');
+    if (picture) {
       const img = picture.querySelector('img');
-
+      
       // Create optimized thumbnail
       const thumbnail = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
       thumbnail.classList.add('lightbox-thumbnail');
-
+      
       // Create optimized full-size image for modal
       const fullImage = createOptimizedPicture(img.src, img.alt, false, [{ width: '2000' }]);
       fullImage.classList.add('lightbox-full');
-
+      
       // Create modal
       const modal = new Modal();
       const modalContent = document.createElement('div');
       modalContent.classList.add('lightbox-modal');
       modalContent.appendChild(fullImage);
-
-      // Replace original elements with thumbnail
-      picture.replaceWith(thumbnail);
-      nextElement.remove();
-
-      // Add click handler
-      thumbnail.addEventListener('click', () => {
+      
+      // Prevent link navigation and show modal instead
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
         modal.dialog = modal.createDialog(modalContent);
         document.body.appendChild(modal.dialog);
         modal.showModal();
